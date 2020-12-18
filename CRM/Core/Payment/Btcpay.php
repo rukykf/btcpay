2d9b6e784882b4834aac83098638bc14b405ed05
Oghenerukevwe Kofi <rukkykofi@fsf.org>
2020-12-18 19:59:58 +0000

    core payment processor

---
+++ CRM/Core/Payment/Btcpay.php
@@ -0,0 +1,229 @@
+<?php
+
+class CRM_Core_Payment_Btcpay extends CRM_Core_Payment {
+
+  use CRM_Core_Payment_BtcpayTrait;
+
+  public static $className = 'Payment_Btcpay';
+
+  /**
+   * @var CRM_Btcpay_Client The Btcpay client object
+   */
+  private $_client = NULL;
+
+  /**
+   * Constructor
+   *
+   * @param string $mode
+   *   The mode of operation: live or test.
+   *
+   * @return void
+   */
+  public function __construct($mode, &$paymentProcessor) {
+    $this->_paymentProcessor = $paymentProcessor;
+    $this->_processorName = ts('Btcpay');
+    $this->_client = new CRM_Btcpay_Client($this->_paymentProcessor);
+  }
+
+  /**
+   * This function checks to see if we have the right config values.
+   *
+   * @return null|string
+   *   The error message if any.
+   */
+  public function checkConfig() {
+    $error = array();
+
+    if (empty($this->_paymentProcessor['password'])) {
+      $error[] = ts('The decryption password has not been set.');
+    }
+
+    if (!empty($error)) {
+      return implode('<p>', $error);
+    }
+    else {
+      return NULL;
+    }
+  }
+
+  /**
+   * We can use the btcpay processor on the backend
+   * @return bool
+   */
+  public function supportsBackOffice() {
+    return FALSE;
+  }
+
+  /**
+   * We can edit recurring contributions
+   * @return bool
+   */
+  public function supportsEditRecurringContribution() {
+    return FALSE;
+  }
+
+  /**
+   * We can configure a start date
+   * @return bool
+   */
+  public function supportsFutureRecurStartDate() {
+    return FALSE;
+  }
+
+  /**
+   * Override CRM_Core_Payment function
+   *
+   * @return array
+   */
+  public function getPaymentFormFields() {
+    // Btcpay loads a payment modal via JS, we don't need any payment fields
+    return [];
+  }
+
+  /**
+   * Return an array of all the details about the fields potentially required for payment fields.
+   * Only those determined by getPaymentFormFields will actually be assigned to the form
+   *
+   * @return array
+   *   field metadata
+   */
+  public function getPaymentFormFieldsMetadata() {
+    // Btcpay loads a payment modal via JS, we don't need any payment fields
+    return [];
+  }
+
+  /**
+   * Get form metadata for billing address fields.
+   *
+   * @param int $billingLocationID
+   *
+   * @return array
+   *    Array of metadata for address fields.
+   */
+  public function getBillingAddressFieldsMetadata($billingLocationID = NULL) {
+    // Btcpay loads a payment modal via JS, we don't need any billing fields - could optionally add some though?
+    return [];
+  }
+
+  public function getBillingAddressFields($billingLocationID = NULL) {
+    // Btcpay loads a payment modal via JS, we don't need any billing fields - could optionally add some though?
+    return [];
+  }
+
+  /**
+   * Process payment
+   * Submit a payment using Btcpay's PHP API:
+   * //TODO put correct link to BTCPayServer client code
+   *
+   * Payment processors should set payment_status_id and trxn_id (if available).
+   *
+   * @param array $params
+   *   Assoc array of input parameters for this transaction.
+   *
+   * @param string $component
+   *
+   * @return array
+   *   Result array
+   *
+   * @throws \CRM_Core_Exception
+   * @throws \CiviCRM_API3_Exception
+   */
+  public function doPayment(&$params, $component = 'contribute') {
+    // Get the btcpay client object
+    $client = $this->_client->getClient();
+
+    /**
+     * This is where we will start to create an Invoice object, make sure to check
+     * the InvoiceInterface for methods that you can use.
+     */
+    $invoice = new \BTCPayServer\Invoice();
+    $buyer = new \BTCPayServer\Buyer();
+    $buyer->setEmail($this->getBillingEmail($params, $this->getContactId($params)));
+    // Add the buyers info to invoice
+    $invoice->setBuyer($buyer);
+    /**
+     * Item is used to keep track of a few things
+     */
+    $item = new \BTCPayServer\Item();
+    $item
+      ->setCode(CRM_Utils_Array::value('item_name', $params))
+      ->setDescription($params['description'])
+      ->setPrice($this->getAmount($params));
+    $invoice->setItem($item);
+
+    /* You can configure the exchange rate provider that BTCPayServer uses from your store */
+
+    $invoice->setCurrency(new \BTCPayServer\Currency($this->getCurrency($params)));
+    // Configure the rest of the invoice
+    $invoice
+      ->setOrderId($params['contributionID'])
+      // You will receive IPN's at this URL, should be HTTPS for security purposes!
+      ->setNotificationUrl($this->getNotifyUrl());
+    /**
+     * Updates invoice with new information such as the invoice id and the URL where
+     * a customer can view the invoice.
+     */
+    try {
+      $client->createInvoice($invoice);
+    } catch (\Exception $e) {
+      $msg = "Btcpay doPayment Exception occured: " . $e->getMessage().PHP_EOL;
+      $request  = $client->getRequest();
+      $response = $client->getResponse();
+      $msg .= (string) $request.PHP_EOL.PHP_EOL.PHP_EOL;
+      $msg .= (string) $response.PHP_EOL.PHP_EOL;
+      Civi::log()->debug($msg);
+      Throw new CRM_Core_Exception($msg);
+    }
+    Civi::log()->debug('invoice created: ' . $invoice->getId(). '" url: ' . $invoice->getUrl() . ' Verbose details: ' . print_r($invoice, TRUE));
+
+    // Success!
+    // For contribution workflow we have a contributionId so we can set parameters directly.
+    // For events/membership workflow we have to return the parameters and they might get set...
+    $newParams['trxn_id'] = $invoice->getId();
+    $newParams['payment_status_id'] = CRM_Core_PseudoConstant::getKey('CRM_Contribute_BAO_Contribution', 'contribution_status_id', 'Pending');
+    // $newParams['fee_amount'] = ...
+    // $newParams['net_amount'] = ...
+
+    if ($this->getContributionId($params)) {
+      $newParams['id'] = $this->getContributionId($params);
+      civicrm_api3('Contribution', 'create', $newParams);
+      unset($newParams['id']);
+    }
+    $params = array_merge($params, $newParams);
+
+    return $params;
+
+  }
+
+  /**
+   * Default payment instrument validation.
+   *
+   * Implement the usual Luhn algorithm via a static function in the CRM_Core_Payment_Form if it's a credit card
+   * Not a static function, because I need to check for payment_type.
+   *
+   * @param array $values
+   * @param array $errors
+   */
+  public function validatePaymentInstrument($values, &$errors) {
+    // Use $_POST here and not $values - for webform fields are not set in $values, but are in $_POST
+    CRM_Core_Form::validateMandatoryFields($this->getMandatoryFields(), $_POST, $errors);
+  }
+
+  /**
+   * Process incoming payment notification (IPN).
+   * https://bitpay.com/docs/invoice-callbacks //TODO put correct link in here
+   *
+   * @throws \CRM_Core_Exception
+   * @throws \CiviCRM_API3_Exception
+   */
+  public static function handlePaymentNotification() {
+    $dataRaw = file_get_contents("php://input");
+    $data = json_decode($dataRaw);
+    $ipnClass = new CRM_Core_Payment_BtcpayIPN($data);
+    if ($ipnClass->main()) {
+      //Respond with HTTP 200, so BTCPay knows the IPN has been received correctly
+      http_response_code(200);
+    }
+  }
+
+}
---
+++ CRM/Core/Payment/BtcpayIPN.php
@@ -0,0 +1,121 @@
+<?php
+
+class CRM_Core_Payment_BtcpayIPN extends CRM_Core_Payment_BaseIPN {
+
+  use CRM_Core_Payment_BtcpayIPNTrait;
+
+  /**
+   * @var CRM_Btcpay_Client The Btcpay client object
+   */
+  private $_client = NULL;
+
+  /**
+   * @var \BTCPayServer\Invoice
+   */
+  private $_invoice = NULL;
+
+  /**
+   * CRM_Core_Payment_BtcpayIPN constructor.
+   *
+   * @param array $ipnData
+   * @param bool $verify
+   *
+   * @throws \CRM_Core_Exception
+   */
+  public function __construct($ipnData) {
+    $this->setInputParameters($ipnData);
+    parent::__construct();
+  }
+
+  public function setInputParameters($ipnData) {
+    // Get the payment processor
+    $this->getPaymentProcessor();
+
+    // Get the btcpay client
+    $this->_client = new CRM_Btcpay_Client($this->_paymentProcessor);
+    $client = $this->_client->getClient();
+
+    // Now fetch the invoice from BTCPay
+    // This is needed, since the IPN does not contain any authentication
+    $invoice = $client->getInvoice($ipnData->id);
+    $this->_invoice = $invoice;
+
+    // FIXME: this is for debug, we could remove it...
+    $invoiceId = $invoice->getId();
+    $invoiceStatus = $invoice->getStatus();
+    $invoiceExceptionStatus = $invoice->getExceptionStatus();
+    $invoicePrice = $invoice->getPrice();
+    Civi::log()->debug("IPN received for Btcpay invoice ".$invoiceId." . Status = " .$invoiceStatus." / exceptionStatus = " . $invoiceExceptionStatus." Price = ". $invoicePrice. "\n");
+    Civi::log()->debug("Raw IPN data: ". print_r($ipnData, TRUE));
+  }
+
+  /**
+   * Main handler for btcpay IPN callback
+   *
+   * @return bool
+   * @throws \CiviCRM_API3_Exception
+   */
+  public function main() {
+    // First we receive an IPN with status "paid" - contribution remains pending - how do we indicate we received "paid"?
+    // Then we receive an IPN with status "confirmed" - we set contribution = completed.
+
+    switch ($this->_invoice->getStatus()) {
+      case \BTCPayServer\Invoice::STATUS_NEW:
+        // We don't do anything in this state
+        return TRUE;
+
+      case \BTCPayServer\Invoice::STATUS_EXPIRED:
+        // Mark as cancelled
+        $this->canceltransaction(['id' => $this->getContributionId(), $this->_paymentProcessor['id']]);
+        break;
+
+      case \BTCPayServer\Invoice::STATUS_INVALID:
+        // Mark as failed
+        $this->failtransaction(['id' => $this->getContributionId(), $this->_paymentProcessor['id']]);
+        break;
+
+      case \BTCPayServer\Invoice::STATUS_PAID:
+        // Remain in pending status
+        // FIXME: Should we record the paid status?
+        return TRUE;
+
+      case \BTCPayServer\Invoice::STATUS_CONFIRMED:
+        // Mark payment as completed
+        civicrm_api3('Contribution', 'completetransaction', [
+          'id' => $this->getContributionId(),
+          'trxn_date' => $this::$_now,
+          'is_email_receipt' => 0,
+        ]);
+        break;
+
+      case \BTCPayServer\Invoice::STATUS_COMPLETE:
+        // Don't do anything, confirmed is ok.
+        return TRUE;
+
+    }
+  }
+
+  /**
+   * @return int Contribution ID
+   */
+  private function getContributionId() {
+    try {
+      return civicrm_api3('Contribution', 'getvalue', [
+        'return' => "id",
+        'trxn_id' => $this->_invoice->getId(),
+        'contribution_test' => $this->_paymentProcessor['is_test'],
+      ]);
+    }
+    catch (Exception $e) {
+      $this->exception('Could not find contribution ID for invoice ' . $this->_invoice->getId());
+    }
+  }
+
+  private function exception($message) {
+    $errorMessage = 'BtcpayIPN Exception: Error: ' . $message;
+    Civi::log()->debug($errorMessage);
+    http_response_code(400);
+    exit(1);
+  }
+
+}
